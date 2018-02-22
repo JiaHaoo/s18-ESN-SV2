0eebb203293af7738424c28c78612ef5912f320e
Yizhao Han <yizhaohan@Yizhaos-MacBook-Pro.local>
2018-02-21 16:47:49 -0800

    add user list api

--- ESN/routes/users.js
+++ ESN/routes/users.js
@@ -3,15 +3,26 @@ var router = express.Router();
 var User = require('../models/models.js').User;
 var passport = require('passport');
 
-
 /* GET users listing. */
-router.get(/([a-z0-9A-Z_])+-*([a-z0-9A-Z_])+/, function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', passport.authenticate('local'), function(req, res, next) {
+    User.
+    find({}).
+    sort('username').
+    exec(function(err,alluser){
+        //  var onlines=[];
+        //  var offlines=[];
+
+        onlines=alluser.filter(function(user){
+            return user.status === 'online'
+        });
+        offlines=alluser.filter(function(user){
+            return user.status === 'offline'
+        });
+        onl_map = onlines.map(x => x.username);
+        offl_map = offlines.map(x => x.username);
+        res.json(200, {online: onl_map, offline: offl_map});
+    });
 });
-
-// router.get(/a/, function(req, res, next) {
-
-// });
 
 // Post Login Info
 router.post('/', passport.authenticate('local'), function(req, res) {

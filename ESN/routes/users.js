9e129048abb7b6c69bb7319f7349a8294076aed6
Changjiang <changjiang.zhong@sv.cmu.edu>
2018-02-22 15:12:09 -0800

    add loggedIn middleware to Show Users API router

--- ESN/routes/users.js
+++ ESN/routes/users.js
@@ -49,7 +49,7 @@ module.exports = function (io) {
     );
 
     // Show Users
-    router.get('/', function(req, res, next) {
+    router.get('/', loggedIn, function(req, res, next) {
         var sorts = req.query.sort;
         if(!sorts) {
             // Specify in the sort parameter the field or fields to sort by 

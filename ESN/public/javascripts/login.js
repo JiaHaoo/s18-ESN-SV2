bd2b70d266e918a95b46876c92e3e2c47b974d76
Changjiang <changjiang.zhong@sv.cmu.edu>
2018-02-22 02:47:03 -0800

    use MD5 to encrypt password before sending

--- ESN/public/javascripts/login.js
+++ ESN/public/javascripts/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#signin-btn').click(function() {
     	var un = $('#username-input').val();
-    	var ps = $('#password-input').val();
+    	var ps = md5($('#password-input').val());
     	// check the correctness of username
 
         login(un, ps);
@@ -9,27 +9,27 @@ $(document).ready(function() {
 
     $('#register-btn').click(function() {
 		var un = $('#username-input').val();
-    	var ps = $('#password-input').val();
+    	var ps = md5($('#password-input').val());
 
     	$.ajax({
     		type: 'PUT',
     		url: '/v1/users/'+un,
     		data: {password: ps},
     		success: function(res) {
-    			login(un, ps);
+    			login(un, ps, true);
     		}
     	});
     });
 });
 
 
-function login(username, password) {
+function login(username, password, newFlag) {
 	$.ajax({
 		type: 'POST',
 		url: '/v1/users',
 		data: {username: username, password: password},
 		success: function(res) {
-			window.location.href = res['redirect'];
+			window.location.href = res['redirect'] + '?newMember' + (newFlag ? '1' : '0');
 		}
 	}).fail(function($xhr) {
 		var data = $xhr.responseJSON;

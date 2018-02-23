548656cb0eaa4f3a05289dac20dbe8f5ce7a6ccb
xplorld <xplorld@gmail.com>
2018-02-22 17:22:44 -0800

    add passport-socket.io

--- ESN/app.js
+++ ESN/app.js
@@ -28,14 +28,25 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+var expresssession = require('express-session');
+//this option used for both express-session and passport-socketio
+var expressoptions = {
+  key: 'connect.sid',
+  secret: 'keyboard cat',
+  store: new expresssession.MemoryStore(),
+  resave: false,
+  saveUninitialized: false,
+  cookieParser: cookieParser,
+  passport: passport
+};
 app.use(cookieParser());
-app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(expresssession(expressoptions));
 app.use(passport.initialize());
 app.use(passport.session());
+
+io.use(require('passport.socketio').authorize(expressoptions));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -52,14 +63,14 @@ passport.deserializeUser(User.deserializeUser());
 mongoose.connect('mongodb://127.0.0.1/ESN');
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

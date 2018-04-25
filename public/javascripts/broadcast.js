dcac0f0a5262f58bd7f1aec8c51b0843a41a9c19
xplorld <xplorld@gmail.com>
2018-04-25 09:14:01 -0700

    logout inactive user

--- public/javascripts/broadcast.js
+++ public/javascripts/broadcast.js
@@ -1,12 +1,12 @@
-$('document').ready(function() {
+$('document').ready(function () {
     var socket = io();
     console.log('before socket');
-    
+
     //var alert_id = '#logout-alert';
     //console.log($(alert_id).find('p').text());
     //console.log($('#content').text());
 
-    socket.on('show_logout_message', function(msg) {
+    socket.on('show_logout_message', function (msg) {
         console.log('in socket');
         var alert_id = '#logout-alert';
         write_alert(alert_id, msg);
@@ -14,22 +14,19 @@ $('document').ready(function() {
     });
 });
 
-function write_alert(alert_id, msg)
-{
+function write_alert(alert_id, msg) {
     console.log('before writing alert: ');
     console.log($(alert_id).find('p').text());
     $(alert_id).find('p').text(msg);
     console.log($(alert_id).find('p').text());
 }
 
-function logout()
-{
+function logout() {
     window.location.href = '/signout';
     $('#logout-alert').hide();
 }
 
-function show_alert()
-{
-    $('#logout-alert').show();
+function show_alert() {
+    $('#logout-alert').modal({ show: true, backdrop: 'static', keyboard: false });
 }
 

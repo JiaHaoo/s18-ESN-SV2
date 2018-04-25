4e1164eff7b706bc4bb36e2e994a5d7105bd3b75
Changjiang <changjiang.zhong@sv.cmu.edu>
2018-04-25 03:33:45 -0700

    fix bugs

--- test/announcements_api_it.js
+++ test/announcements_api_it.js
@@ -30,23 +30,39 @@ describe('test /v1/announcements', () => {
         //2. register
         //3. log in
 
+        // cleanDatabase.cleanDatabase()
+        //     .then(() => {
+        //         return agent
+        //             .post('/v1/users/' + username) //register
+        //             .send({ username: username, password: password });
+        //     })
+        //     .then(() => {
+        //         return agent
+        //             .post('/v1/users') // log in
+        //             .send({ username: username, password: password });
+        //     })
+        //     .then((res) => {
+        //         //res is response of `/v1/users`
+        //         expect(res).to.have.status(200);
+        //         expect(res).to.have.cookie('connect.sid');
+        //         // The `agent` now has the sessionid cookie saved, and will send it
+        //         // back to the server in the next request
+        //     })
+        //     .then(done)
+        //     .catch((err) => console.log(err));
+
         cleanDatabase.cleanDatabase()
-            // .then(() => {
-            //     return agent
-            //         .post('/v1/users/' + username) //register
-            //         .send({ username: username, password: password });
-            // })
+            .then(() => {
+                User.create({ username: username, password: password, privilege_level: 'Administrator' });
+            })
             .then(() => {
                 return agent
-                    .post('/v1/users') // log in
+                    .post('/v1/users')
                     .send({ username: username, password: password });
             })
             .then((res) => {
-                //res is response of `/v1/users`
                 expect(res).to.have.status(200);
                 expect(res).to.have.cookie('connect.sid');
-                // The `agent` now has the sessionid cookie saved, and will send it
-                // back to the server in the next request
             })
             .then(done)
             .catch((err) => console.log(err));
